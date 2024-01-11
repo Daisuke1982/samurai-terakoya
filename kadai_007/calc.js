cf988f82e92b6ab741f9e6bb3a08513f6bcfae80
Dai-tan <128608244+Daisuke1982@users.noreply.github.com>
2024-01-11 09:09:51 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+let d = new Date();
+let num = Math.ceil(Math.random() * 5) * d.getSeconds();
+
+console.log(num);
+
+if ((num % 15) === 0) {
+  console.log('15の倍数です')
+}
+else if((num % 3) === 0){
+  console.log('3の倍数です');
+}  
+else if((num % 5) === 0){
+  console.log('5の倍数です') ;
+}
+else {
+  console.log(num);
+}
